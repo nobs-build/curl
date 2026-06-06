ebe9ebbfa9fe909900c04d2c72fb7510af35a0d9
Jirka Koutny <jirka.koutny@protonmail.com>
2026-06-06 10:51:47 +0200

    Attempt to apply new way of instructing ai to decompose and recompose projects.

---
+++ core/platform.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_CURL_PLATFORM_H
+#define HEADER_CURL_PLATFORM_H
+
+#include "curl_setup.h"
+
+CURLcode Curl_platform_init(void);
+void Curl_platform_cleanup(void);
+
+#endif
---
+++ plat/linux/dummy.c
@@ -0,0 +1,11 @@
+
+#include "platform.h"
+
+CURLcode Curl_platform_init(void)
+{
+  return CURLE_OK;
+}
+
+void Curl_platform_cleanup(void)
+{
+}
